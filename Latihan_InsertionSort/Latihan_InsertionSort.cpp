e9e0782561607ef40daabfcbea064881a05c3027
namira121 <127211008+namira121@users.noreply.github.com>
2023-04-07 08:18:51 +0700

    Update Latihan_InsertionSort.cpp

--- Latihan_InsertionSort/Latihan_InsertionSort.cpp
+++ Latihan_InsertionSort/Latihan_InsertionSort.cpp
@@ -1,20 +1,72 @@
-// Latihan_InsertionSort.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int arr[20];			// membuat array dengan panjang data 20
+int n;					// membuat variable inputan n
+
+void input() {		// Procedure Input
+	while (true) {
+		cout << "Masukkan jumlah data pada array : "; // Membuat Inputan jumlah element Array
+		cin >> n;									// Memanggil variable inputan n
+
+		if (n <= 20) {								// membuat kondisi n tidak lebih dari 20
+			break;
+		}
+		else 
+		{
+			cout << "\nArray yang anda masukkan maksimal 20 elemen.\n"; // Menampilkan pesan jika data lebih dari 20
+		}
+	} 
+	cout << endl;								// Membuat jarak per baris program
+	cout << "======================" << endl;   // Membuat tampilan susunan data element array
+	cout << "Masukkan Element Array" << endl;
+	cout << "======================" << endl;
+
+	for (int i = 0; i < n; i++)					// Menggunakan perulangan for untuk menyimpan data pada array
+	{
+		cout << "data ke-" << (i + 1) << ": ";
+		cin >> arr[i];
+	}
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void insertionsort() {    // Menambahkan prosedur Insertion Sort
+	int temp;			// Membuat variable data temporer atau penyimpanan sementara
+	int j;				// Membuat variable j sebagai penanda
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	for (int i = 1; i < n; i++) {  // 1. Looping dengan dimulai dari 1 hingga n-1
+
+		temp = arr[i];   // 2. Simpan nilai arr[i] ke variable sementara temp
+
+		j = i - 1;		// 3. Setting nilai j sama dengan i-1;
+
+		while (j >= 0 && arr[j] > temp) // 4. Looping while dimana nilai j lebih besar sama dengan 0 dan 
+										// arr[j] lebih besar daripada temp
+		{
+			arr[j + 1] = arr[j];		// a. simpan arr[j] ke dalam nilai variable arr[j+1]
+			j--;						// b. Decrement nilai j by 1 
+		}
+		arr[j + 1] = temp;				// 5. simpan nilai temp ke dalam arr[j+1]
+
+		cout << "\nPass " << i << ": "; // Output ke layar
+		for (int k = 0; k < n; k++) {	// Looping nilai k dimulai dari 0 hingga n-1
+			cout << arr[k] << " ";		// Output ke layar
+		}
+	}
+}
+
+void display() {   // Menambahkan prosedur display
+	cout << "\n===============================" << endl;  // Membuat tampilan element Array yang telah tersusun
+	cout << "Element Array Yang Telah Tersusun" << endl;
+	cout << "=================================" << endl;
+
+	for (int j = 0; j < n; j++) {  // Menggunakan for untuk perulangan menyimpan data 
+		cout << arr[j] << " ";
+	}
+}
+
+int main()  // Fungsi untuk menjalankan program
+{
+	input();
+	insertionsort();
+	display();
+}
